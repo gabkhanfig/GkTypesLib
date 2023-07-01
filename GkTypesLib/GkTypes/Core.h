de46b663095d01f973fa5f217e9d9a4a88d1d420
gabkhanfig <gabkhanfig@gmail.com>
2023-07-01 16:19:02 -0400

    AVX2 Enhanced Comparison Checking

---
+++ GkTypesLib/GkTypes/Core.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#ifndef GK_CHECK
+#define GK_CHECK true
+#endif
--- GkTypesLib/GkTypes/Utility.h
+++ GkTypesLib/GkTypes/Utility.h
@@ -1,9 +1,13 @@
 #pragma once
 
+#include "Core.h"
 #include <Windows.h>
 #include <psapi.h>
 #include <libloaderapi.h>
 #include <string>
+#include "BasicTypes.h"
+#include <immintrin.h>
+#include <iostream>
 
 namespace gk
 {
@@ -24,6 +28,11 @@ namespace gk
 		return std::char_traits<char>::length(str);
 	}
 
+	[[nodiscard]] static uint64 strnlen(const char* str, uint64 maxSize) {
+		const char* found = (const char*)memchr(str, '\0', maxSize);
+		return found ? (uint64)(found - str) : maxSize;
+	}
+
 	[[nodiscard]] constexpr static bool StrEqual(const char* str1, const char* str2, size_t num) {
 		if (str1 == str2) {
 			return true;
@@ -35,4 +44,43 @@ namespace gk
 		}
 		return true;
 	}
+
+	[[nodiscard]] constexpr uint64 UpperPowerOfTwo(uint64 v) {
+		v--;
+		v |= v >> 1;
+		v |= v >> 2;
+		v |= v >> 4;
+		v |= v >> 8;
+		v |= v >> 16;
+		v |= v >> 32;
+		v++;
+		return v;
+	}
+
+	[[nodiscard]] static bool isAligned(const void* ptr, uint64 alignment) noexcept {
+		auto iptr = reinterpret_cast<uint64>(ptr);
+		return !(iptr % alignment);
+	}
+
+	/* Expects 32 byte alignment */
+	[[nodiscard]] static bool AVX2CheckEqual32ByteBlocks(const void* left, const void* right) {
+#if GK_CHECK
+		if (!isAligned(left, 32)) {
+			std::cout << uint64(left);
+			DebugBreak();
+		}
+		if (!isAligned(right, 32)) {
+			std::cout << uint64(right);
+			DebugBreak();
+		}
+#endif
+		const __m256i vectorLeft = _mm256_loadu_epi8(left);
+		const __m256i vectorRight = _mm256_loadu_epi8(right);
+		const __m256i compareResult = _mm256_cmpeq_epi8(vectorLeft, vectorRight);
+		const int bitmask = _mm256_movemask_epi8(compareResult);
+		return bitmask == -1; // all bits flipped (~0)
+	}
+
+
+
 }
