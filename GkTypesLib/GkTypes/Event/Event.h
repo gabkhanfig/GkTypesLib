0c2390c64e76c730be14d7e6c37f9c319bfe42ae
gabkhanfig <gabkhanfig@gmail.com>
2023-07-14 16:49:37 -0400

    Events Return Support

--- GkTypesLib/GkTypes/Event/Event.h
+++ GkTypesLib/GkTypes/Event/Event.h
@@ -6,43 +6,56 @@
 namespace gk 
 {
 
-	template<typename... Types>
+	template<typename ReturnT, typename... Types>
 	class Event;
 
 	namespace {
-		template<typename T, typename... Types>
-		static gk::Event<Types...>* _MakeEvent(T* obj, void(T::* func)(Types...));
+		template<typename T, typename ReturnT, typename... Types>
+		static gk::Event<ReturnT, Types...>* _MakeEvent(T* obj, ReturnT(T::* func)(Types...));
 
-		template<typename... Types>
-		static gk::Event<Types...>* _MakeEvent(void(*func)(Types...));
+		template<typename T, typename ReturnT, typename... Types>
+		static gk::Event<ReturnT, Types...>* _MakeEvent(T* obj, ReturnT(T::* func)(Types...)const);
+
+		template<typename ReturnT, typename... Types>
+		static gk::Event<ReturnT, Types...>* _MakeEvent(ReturnT(*func)(Types...));
 	}
 
-	/* Event that can callback a function (optionally a member function of a specific object). 
+	/* Event that can callback a function (optionally a member function of a specific object).
+	The first argument is the function return type.
 	The variadic template arguments correspond to the arguments for both the member and non-member functions.
-	For example, gk::event<int, float> would correspond to an event that takes an int and float parameters.
+	For example, gk::event<void, int, float> would correspond to an event that takes an int and float parameters and returns nothing.
 	Member functions do not need the invisible *this argument specified. */
-	template<typename... Types>
+	template<typename ReturnT, typename... Types>
 	class Event {
 	public:
 
-		virtual void Invoke(Types... vars) = 0;
+		[[nodiscard]] virtual ReturnT Invoke(Types... vars) = 0;
 
-		/* This function will assert if the event is created without an object. */
+		/* This function will assert if the event is created without an object. 
+		This function will also assert if the event is created with a const object / function. */
 		template<typename T>
 		T* Obj() const { return (T*)GetObj(); }
 
-		/* Creates an event that is bound to a specific object. Use Obj() to get the internal object. 
-		See the other overload for Create(). */
+		/* Creates an event that is bound to a specific object. Use Obj() to get the internal object.
+		Explicitly uses non-const pointers to objects. */
 		template<typename T>
-		static Event* Create(T* obj, void (T::* func)(Types...)) {
+		static Event* Create(T* obj, ReturnT (T::* func)(Types...)) {
 			gk_assertNotNull(obj);
-			return _MakeEvent<T, Types...>(obj, func);
+			return _MakeEvent<T, ReturnT, Types...>(obj, func);
 		}
 
-		/* Creates an event that is not bound to an object. Calling Obj() will throw an assertion if GK_CHECK is true.
+		/* Creates an event that is bound to a specific object's const member function. Use Obj() to get the internal object.
+		Explicitly uses non-const pointers to objects. */
+		template<typename T>
+		static Event* Create(T* obj, ReturnT(T::* func)(Types...) const) {
+			gk_assertNotNull(obj);
+			return _MakeEvent<T, ReturnT, Types...>(obj, func);
+		}
+
+		/* Creates an event that is not bound to an object. Calling Obj() or ConstObj() will throw an assertion.
 		See the other overload for Create(). */
-		static Event* Create(void(*func)(Types...)) {
-			return _MakeEvent<Types...>(func);
+		static Event* Create(ReturnT(*func)(Types...)) {
+			return _MakeEvent<ReturnT, Types...>(func);
 		}
 
 	private:
@@ -53,17 +66,17 @@ namespace gk
 
 	namespace {
 
-		template<typename T, typename... Types>
-		class _EventObjectImpl : public Event<Types...>
+		template<typename T, typename ReturnT, typename... Types>
+		class _EventObjectImpl : public Event<ReturnT, Types...>
 		{
-			typedef void (T::* MemberFunc)(Types...);
+			typedef ReturnT (T::* MemberFunc)(Types...);
 
 		public:
 
 			_EventObjectImpl(T* obj, MemberFunc func) : _obj(obj), _func(func) {}
 
-			virtual void Invoke(Types... vars) override {
-				(_obj->*_func)(vars...);
+			[[nodiscard]] virtual ReturnT Invoke(Types... vars) override {
+				return (_obj->*_func)(vars...);
 			}
 
 		private:
@@ -76,17 +89,40 @@ namespace gk
 			MemberFunc _func;
 		};
 
-		template<typename... Types>
-		class _EventImpl : public Event<Types...>
+		template<typename T, typename ReturnT, typename... Types>
+		class _EventObjectImplConst : public Event<ReturnT, Types...>
 		{
-			typedef void (*Func)(Types...);
+			typedef ReturnT(T::* MemberFunc)(Types...) const;
+
+		public:
+
+			_EventObjectImplConst(T* obj, MemberFunc func) : _obj(obj), _func(func) {}
+
+			[[nodiscard]] virtual ReturnT Invoke(Types... vars) override {
+				return (_obj->*_func)(vars...);
+			}
+
+		private:
+
+			virtual void* GetObj() const override { return (void*)_obj; }
+
+		private:
+
+			T* _obj;
+			MemberFunc _func;
+		};
+
+		template<typename ReturnT, typename... Types>
+		class _EventImpl : public Event<ReturnT, Types...>
+		{
+			typedef ReturnT (*Func)(Types...);
 
 		public:
 
 			_EventImpl(Func func) : _func(func) {}
 
-			virtual void Invoke(Types... vars) override {
-				_func(vars...);
+			[[nodiscard]] virtual ReturnT Invoke(Types... vars) override {
+				return _func(vars...);
 			}
 
 		private:
@@ -101,13 +137,18 @@ namespace gk
 			Func _func;
 		};
 
-		template<typename T, typename... Types>
-		inline static gk::Event<Types...>* _MakeEvent(T* obj, void(T::* func)(Types...)) {
+		template<typename T, typename ReturnT, typename... Types>
+		inline static gk::Event<ReturnT, Types...>* _MakeEvent(T* obj, ReturnT(T::* func)(Types...)) {
 			return new _EventObjectImpl(obj, func);
 		}
 
-		template<typename... Types>
-		inline static gk::Event<Types...>* _MakeEvent(void(*func)(Types...)) {
+		template<typename T, typename ReturnT, typename... Types>
+		static gk::Event<ReturnT, Types...>* _MakeEvent(T* obj, ReturnT(T::* func)(Types...)const) {
+			return new _EventObjectImplConst(obj, func);
+		}
+
+		template<typename ReturnT, typename... Types>
+		inline static gk::Event<ReturnT, Types...>* _MakeEvent(ReturnT(*func)(Types...)) {
 			return new _EventImpl(func);
 		}
 
