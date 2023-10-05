f28bf9e883a86fb19e5f4a877974ada24a185a5d
gabkhanfig <gabkhanfig@gmail.com>
2023-10-05 02:57:34 -0400

    gk::Event manages it's own memory to eliminate forgetting to free

--- GkTypesLib/GkTypes/Event/Event.h
+++ GkTypesLib/GkTypes/Event/Event.h
@@ -5,153 +5,226 @@
 
 namespace gk 
 {
-
-	template<typename ReturnT, typename... Types>
-	class Event;
-
-	namespace {
-		template<typename T, typename ReturnT, typename... Types>
-		static gk::Event<ReturnT, Types...>* _MakeEvent(T* obj, ReturnT(T::* func)(Types...));
-
-		template<typename T, typename ReturnT, typename... Types>
-		static gk::Event<ReturnT, Types...>* _MakeEvent(T* obj, ReturnT(T::* func)(Types...)const);
-
-		template<typename ReturnT, typename... Types>
-		static gk::Event<ReturnT, Types...>* _MakeEvent(ReturnT(*func)(Types...));
-	}
-
-	/* Event that can callback a function (optionally a member function of a specific object).
+	/* Don't need to allocate with new, manages it's own memory.
+	Event that can callback a function (optionally a member function of a specific object).
 	The first argument is the function return type.
 	The variadic template arguments correspond to the arguments for both the member and non-member functions.
 	For example, gk::event<void, int, float> would correspond to an event that takes an int and float parameters and returns nothing.
 	Member functions do not need the invisible *this argument specified. */
 	template<typename ReturnT, typename... Types>
-	class Event {
-	public:
-
-		[[nodiscard]] virtual ReturnT Invoke(Types... vars) = 0;
-
-		/* This function will assert if the event is created without an object. 
-		This function will also assert if the event is created with a const object / function. */
-		template<typename T>
-		T* Obj() const { return (T*)GetObj(); }
-
-		/* Creates an event that is bound to a specific object. Use Obj() to get the internal object.
-		Explicitly uses non-const pointers to objects. */
-		template<typename T>
-		static Event* Create(T* obj, ReturnT (T::* func)(Types...)) {
-			gk_assertNotNull(obj);
-			return _MakeEvent<T, ReturnT, Types...>(obj, func);
-		}
-
-		/* Creates an event that is bound to a specific object's const member function. Use Obj() to get the internal object.
-		Explicitly uses non-const pointers to objects. */
-		template<typename T>
-		static Event* Create(T* obj, ReturnT(T::* func)(Types...) const) {
-			gk_assertNotNull(obj);
-			return _MakeEvent<T, ReturnT, Types...>(obj, func);
-		}
-
-		/* Creates an event that is not bound to an object. Calling Obj() or ConstObj() will throw an assertion.
-		See the other overload for Create(). */
-		static Event* Create(ReturnT(*func)(Types...)) {
-			return _MakeEvent<ReturnT, Types...>(func);
-		}
-
+	struct Event {
 	private:
 
-		virtual void* GetObj() const = 0;
-	
-	};
-
-	namespace {
-
-		template<typename T, typename ReturnT, typename... Types>
-		class _EventObjectImpl : public Event<ReturnT, Types...>
-		{
-			typedef ReturnT (T::* MemberFunc)(Types...);
-
+		class _BaseEvent {
 		public:
-
-			_EventObjectImpl(T* obj, MemberFunc func) : _obj(obj), _func(func) {}
-
-			[[nodiscard]] virtual ReturnT Invoke(Types... vars) override {
-				return (_obj->*_func)(vars...);
-			}
-
-		private:
-
-			virtual void* GetObj() const override { return (void*)_obj; }
-
-		private:
-
-			T* _obj;
-			MemberFunc _func;
+			virtual ReturnT invoke(Types... vars) const = 0;
+			virtual bool isObject(const void* obj) const = 0;
+			virtual const _BaseEvent* makeCopy() const = 0;
 		};
 
-		template<typename T, typename ReturnT, typename... Types>
-		class _EventObjectImplConst : public Event<ReturnT, Types...>
-		{
-			typedef ReturnT(T::* MemberFunc)(Types...) const;
-
+		class _EventFreeFuncImpl : public _BaseEvent {
+			typedef ReturnT(*Func)(Types...);
 		public:
-
-			_EventObjectImplConst(T* obj, MemberFunc func) : _obj(obj), _func(func) {}
-
-			[[nodiscard]] virtual ReturnT Invoke(Types... vars) override {
-				return (_obj->*_func)(vars...);
-			}
-
-		private:
-
-			virtual void* GetObj() const override { return (void*)_obj; }
-
-		private:
-
-			T* _obj;
-			MemberFunc _func;
-		};
-
-		template<typename ReturnT, typename... Types>
-		class _EventImpl : public Event<ReturnT, Types...>
-		{
-			typedef ReturnT (*Func)(Types...);
-
-		public:
-
-			_EventImpl(Func func) : _func(func) {}
-
-			[[nodiscard]] virtual ReturnT Invoke(Types... vars) override {
+			_EventFreeFuncImpl(Func func) : _func(func) {}
+			virtual ReturnT invoke(Types... vars) const override {
 				return _func(vars...);
 			}
-
-		private:
-
-			virtual void* GetObj() const override {
-				gk_assertm(false, "Cannot get the object for an event that does not use a member function");
-				return nullptr;
+			virtual bool isObject(const void* obj) const override {
+				return false;
 			}
-
-		private:
+			virtual const _BaseEvent* makeCopy() const override {
+				return new _EventFreeFuncImpl(_func);
+			}
 
 			Func _func;
 		};
 
-		template<typename T, typename ReturnT, typename... Types>
-		inline static gk::Event<ReturnT, Types...>* _MakeEvent(T* obj, ReturnT(T::* func)(Types...)) {
-			return new _EventObjectImpl(obj, func);
+		template <typename ObjT, typename FuncClassT>
+		class _EventObjectImpl : public _BaseEvent {
+			typedef ReturnT(FuncClassT::* MemberFunc)(Types...);
+		public:
+			_EventObjectImpl(ObjT* obj, MemberFunc func) : _obj(obj), _func(func) {}
+			virtual ReturnT invoke(Types... vars) const override {
+				return ((FuncClassT*)_obj->*_func)(vars...);
+			}
+			virtual bool isObject(const void* obj) const override {
+				return _obj == obj;
+			}
+			virtual const _BaseEvent* makeCopy() const override {
+				return new _EventObjectImpl(_obj, _func);
+			}
+
+			ObjT* _obj;
+			MemberFunc _func;
+		};
+
+		template <typename ObjT, typename FuncClassT>
+		class _EventConstObjectImpl : public _BaseEvent {
+			typedef ReturnT(FuncClassT::* MemberFunc)(Types...) const;
+		public:
+			_EventConstObjectImpl(const ObjT* obj, MemberFunc func) : _obj(obj), _func(func) {}
+			virtual ReturnT invoke(Types... vars) const override {
+				return ((const FuncClassT*)_obj->*_func)(vars...);
+			}
+			virtual bool isObject(const void* obj) const override {
+				return _obj == obj;
+			}
+			virtual _BaseEvent* makeCopy() const override {
+				return new _EventConstObjectImpl(_obj, _func);
+			}
+
+			const ObjT* _obj;
+			MemberFunc _func;
+		};
+
+	public:
+
+#pragma region Construct_Destruct_Assign
+
+		Event() : _eventObj(nullptr) {}
+
+		Event(ReturnT(*func)(Types...)) {
+			_eventObj = new _EventFreeFuncImpl(func);
 		}
 
-		template<typename T, typename ReturnT, typename... Types>
-		static gk::Event<ReturnT, Types...>* _MakeEvent(T* obj, ReturnT(T::* func)(Types...)const) {
-			return new _EventObjectImplConst(obj, func);
+		template<typename T>
+		Event(T* obj, ReturnT(T::* func)(Types...)) {
+			_eventObj = new _EventObjectImpl(obj, func);
 		}
 
-		template<typename ReturnT, typename... Types>
-		inline static gk::Event<ReturnT, Types...>* _MakeEvent(ReturnT(*func)(Types...)) {
-			return new _EventImpl(func);
+		template<typename T, typename Child>
+			requires(std::is_base_of_v<T, Child>)
+		Event(T* obj, ReturnT(Child::* func)(Types...)) {
+			_eventObj = new _EventObjectImpl(obj, func);
 		}
 
-	}
+		template<typename T, typename Child>
+			requires(std::is_base_of_v<T, Child>)
+		Event(Child* obj, ReturnT(T::* func)(Types...)) {
+			_eventObj = new _EventObjectImpl(obj, func);
+		}
+
+		template<typename T>
+		Event(const T* obj, ReturnT(T::* func)(Types...) const) {
+			_eventObj = new _EventConstObjectImpl(obj, func);
+		}
+
+		template<typename T, typename Child>
+			requires(std::is_base_of_v<T, Child>)
+		Event(const T* obj, ReturnT(Child::* func)(Types...) const) {
+			_eventObj = new _EventConstObjectImpl(obj, func);
+		}
+
+		template<typename T, typename Child>
+			requires(std::is_base_of_v<T, Child>)
+		Event(const Child* obj, ReturnT(T::* func)(Types...) const) {
+			_eventObj = new _EventConstObjectImpl(obj, func);
+		}
+
+		Event(const Event& other) {
+			_eventObj = other._eventObj->makeCopy();
+		}
+
+		Event(Event&& other) noexcept {
+			_eventObj = other._eventObj;
+			other._eventObj = nullptr;
+		}
+
+		~Event() {
+			freeEventObject();
+		}
+
+		Event& operator = (const Event& other) {
+			freeEventObject();
+			_eventObj = other._eventObj->makeCopy();
+			return *this;
+		}
+
+		Event& operator = (Event&& other) noexcept {
+			_eventObj = other._eventObj;
+			other._eventObj = nullptr;
+			return *this;
+		}
+
+#pragma endregion
+
+#pragma region bind
+
+		void bind(ReturnT(*func)(Types...)) {
+			freeEventObject();
+			_eventObj = new _EventFreeFuncImpl(func);
+		}
+
+		template<typename T>
+		void bind(T* obj, ReturnT(T::* func)(Types...)) {
+			freeEventObject();
+			_eventObj = new _EventObjectImpl(obj, func);
+		}
+
+		template<typename T, typename Child>
+			requires(std::is_base_of_v<T, Child>)
+		void bind(T* obj, ReturnT(Child::* func)(Types...)) {
+			freeEventObject();
+			_eventObj = new _EventObjectImpl(obj, func);
+		}
+
+		template<typename T, typename Child>
+			requires(std::is_base_of_v<T, Child>)
+		void bind(Child* obj, ReturnT(T::* func)(Types...)) {
+			freeEventObject();
+			_eventObj = new _EventObjectImpl(obj, func);
+		}
+		
+		template<typename T>
+		void bind(const T* obj, ReturnT(T::* func)(Types...)const) {
+			freeEventObject();
+			_eventObj = new _EventConstObjectImpl(obj, func);
+		}
+
+		template<typename T, typename Child>
+			requires(std::is_base_of_v<T, Child>)
+		void bind(const T* obj, ReturnT(Child::* func)(Types...) const) {
+			freeEventObject();
+			_eventObj = new _EventConstObjectImpl(obj, func);
+		}
+
+		template<typename T, typename Child>
+			requires(std::is_base_of_v<T, Child>)
+		void bind(const Child* obj, ReturnT(T::* func)(Types...) const) {
+			freeEventObject();
+			_eventObj = new _EventConstObjectImpl(obj, func);
+		}
+
+#pragma endregion
+
+		[[nodiscard]] ReturnT invoke(Types... vars) const {
+			gk_assertm(isBound(), "Event not bound");
+			return _eventObj->invoke(vars...);
+		}
+
+		template<typename T>
+		[[nodiscard]] bool isObject(const T* obj) const {
+			gk_assertm(isBound(), "Event not bound");
+			return _eventObj->isObject((const void*)obj);
+		}
+
+		[[nodiscard]] bool isBound() const {
+			return _eventObj != nullptr;
+		}
+
+	private:
+
+		void freeEventObject() {
+			if (_eventObj != nullptr) {
+				delete _eventObj;
+			}
+		}
+
+	private:
+
+		const _BaseEvent* _eventObj;
+
+	};
+
 	
 }
