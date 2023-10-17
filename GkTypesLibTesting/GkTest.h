8a025e96a55c1edf54e357037b869c286cafbca5
gabkhanfig <gabkhanfig@gmail.com>
2023-10-17 19:12:41 -0400

    Update GkTest.h

--- GkTypesLibTesting/GkTest.h
+++ GkTypesLibTesting/GkTest.h
@@ -8,11 +8,12 @@ COMPTIME_TEST(Number, IsEven, {
 	return (num % 2) == 0; }); 
 */
 #define COMPTIME_TEST(test_case_name, test_name, test_block) \
-consteval bool CompTimeTest_ ## test_case_name ## _ ## test_name() ## test_block \
+consteval bool CompTimeTest_ ## test_case_name ## _ ## test_name() ## { test_block return true; } \
 static_assert(CompTimeTest_ ## test_case_name ## _ ## test_name ## ());
 
-#define comptimeAssert(condition) if((condition) == false) return false;
-#define comptimeAssertEq(v1, v2) if(v1 != v2) return false;
+#define comptimeAssert(condition) if((condition) == false) throw;
+#define comptimeAssertEq(v1, v2) if(v1 != v2) throw;
+#define comptimeAssertNe(v1, v2) if(v1 == v2) throw;
 
 /* https://stackoverflow.com/questions/29174938/googletest-and-memory-leaks
 Only works in debug mode (_DEBUG macro).
