82afc5f4d65529f4a13ec0ea38c256d88bae0e11
gabkhanfig <gabkhanfig@gmail.com>
2023-10-17 19:47:08 -0400

    Unit Tests for Mutex

---
+++ GkTypesLibTesting/Tests/MutexTests.cpp
@@ -0,0 +1,49 @@
+#include "../pch.h"
+#include "../../GkTypesLib/GkTypes/Thread/Mutex.h"
+#include "../GkTest.h"
+#include <thread>
+
+void incrementValue(gk::Mutex<int>* mutex) {
+	auto lock = mutex->lock();
+	(*lock)++;
+}
+
+void runIncrement(gk::Mutex<int>* mutex) {
+	Sleep(50);
+	for (int i = 0; i < 100; i++) {
+		incrementValue(mutex);
+	}
+}
+
+namespace UnitTests
+{
+	TEST(Mutex, SingleThreadsAccess) {
+		gk::Mutex<int> mutex = 0;
+
+		std::thread t1{runIncrement, & mutex};
+
+		// If this test isn't completing, something is wrong.
+
+		t1.join();
+
+		EXPECT_EQ(mutex.getDataNoLock(), 100);
+	}
+
+	TEST(Mutex, MultipleThreadsAccess) {
+		gk::Mutex<int> mutex = 0;
+
+		std::thread t1{runIncrement, & mutex};
+		std::thread t2{runIncrement, & mutex};
+		std::thread t3{runIncrement, & mutex};
+		std::thread t4{runIncrement, & mutex};
+
+		// If this test isn't completing, something is wrong.
+
+		t1.join();
+		t2.join();
+		t3.join();
+		t4.join();
+
+		EXPECT_EQ(mutex.getDataNoLock(), 400);
+	}
+}
