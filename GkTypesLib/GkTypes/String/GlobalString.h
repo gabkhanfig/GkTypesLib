ec9a8bbc881644091aa561dba352b6d5c112494c
gabkhanfig <gabkhanfig@gmail.com>
2023-10-29 01:08:24 -0400

    Updated GlobalString to use gk::HashMap

--- GkTypesLib/GkTypes/String/GlobalString.h
+++ GkTypesLib/GkTypes/String/GlobalString.h
@@ -1,122 +1,143 @@
 #pragma once
 
 #include "String.h"
-#include <unordered_map>
+#include "../Array/DynamicArray.h"
+#include "../Hash/Hashmap.h"
+#include "../Thread/Mutex.h"
 
 namespace gk 
 {
-	/* Runtime global immutable strings. GlobalString internally only holds a pointer, so const-referencing is not necessary. */
+	/* Runtime thread-safe global immutable strings compressed into 4 byte integers. */
 	struct GlobalString
 	{
-		typedef std::unordered_map < gk::String, gk::String* > GlobalStringMapType;
-
 	private:
 
-		inline static GlobalStringMapType stringMap;
+		struct GlobalStringContainers {
+			gk::HashMap<gk::String, uint32> ids;
+			gk::darray<gk::String> strings;
+			uint32 nextId;
+		};
 
-		gk::String* internalString;
+		static gk::Mutex<GlobalStringContainers> initializeGlobalStringContainers() {
+			GlobalStringContainers containers;
+			containers.ids.insert(""_str, 0);
+			containers.strings.Add(""_str);
+			containers.nextId = 1;
+			return gk::Mutex<GlobalStringContainers>(std::move(containers));
+		}
 
-	private:
-
-		/* Will attempt to add a string to the map. If the string map already contains the passed in string, it will instead just get the contained string. */
-		static gk::String* AddStringToMap(const gk::String& str) 
-		{
-			auto search = stringMap.find(str);
-			if (search != stringMap.end()) {
-				return search->second;
-			}
-			gk::String* mapStr = new gk::String(str);
-			stringMap.insert({ str, mapStr });
-			return mapStr;
+		forceinline static gk::Mutex<GlobalStringContainers>& getAllGlobalStrings() {
+			static gk::Mutex<GlobalStringContainers> globalStrings = initializeGlobalStringContainers();
+			return globalStrings;
 		}
 
 	public:
 
-		/* Check if the map contains a specified string. */
-		static bool doesMapContainString(const gk::String& str) 
-		{
-			return stringMap.contains(str);
-		}
-
+		/* Default initializes to an empty string. */ 
 		GlobalString()
-		{
-			internalString = AddStringToMap(""_str);
-		}
-
-		GlobalString(const gk::GlobalString& other)
-		{
-			internalString = other.internalString;
-		}
-
-		GlobalString(const gk::String& str) 
-		{
-			internalString = AddStringToMap(str);
-		}
-
-		GlobalString(const gk::Str& str) 
-		{
-			internalString = AddStringToMap(str);
-		}
-
-		~GlobalString() 
-		{
-
-		}
-
-		GlobalString& operator = (const gk::String& str) 
-		{
-			internalString = AddStringToMap(str);
-			return *this;
-		}
-
-		GlobalString& operator = (GlobalString str) 
-		{
-			internalString = str.internalString;
-			return *this;
-		}
-
-		bool operator == (const gk::Str& str) const
-		{
-			gk::String _str{ str };
-			return *this == _str;
-		}
-
-		bool operator == (const gk::String& str) const
-		{
-			auto search = stringMap.find(str);
-			if (search == stringMap.end()) {
-				return false;
+			: stringId(0)
+		{}
+	
+		/* Creates a new entry into the global string map if the specified string doesn't exist. 
+		If it does exist, uses the pre-existing one. Will copy the string parameter.
+		Setting the template parameter to ThreadSafety::Unsafe will not lock the global string mutex. */
+		template<ThreadSafety safety = ThreadSafety::Safe>
+		static GlobalString create(gk::String inString) {
+			if constexpr (safety == ThreadSafety::Safe) {
+				gk::LockedMutex<GlobalStringContainers> lock = getAllGlobalStrings().lock();
+				GlobalStringContainers& containers = *lock;
+				return createNewEntryIfDoesntExist(std::move(inString), containers);
 			}
-
-			return internalString == search->second;
+			// thread unsafe
+			GlobalStringContainers& containers = getAllGlobalStrings().getDataNoLock();
+			return createNewEntryIfDoesntExist(std::move(inString), containers);
 		}
 
-		bool operator == (GlobalString str) const
-		{
-			return internalString == str.internalString;
+		/* Does not create a new entry. Will be empty string if the string doesn't already exist. 
+		Setting the template parameter to ThreadSafety::Unsafe will not lock the global string mutex. */
+		template<ThreadSafety safety = ThreadSafety::Safe>
+		static GlobalString createIfExists(const gk::String& inString) {
+			GlobalString gstrOut;
+			if constexpr (safety == ThreadSafety::Safe) {
+				gk::LockedMutex<GlobalStringContainers> lock = getAllGlobalStrings().lock();
+				const GlobalStringContainers& containers = *lock;
+				return createFromExistingEntryIfExist(inString, containers);
+			}
+			// thread unsafe
+			const GlobalStringContainers& containers = getAllGlobalStrings().getDataNoLock();
+			return createFromExistingEntryIfExist(inString, containers);
 		}
 
-		gk::String toString() const
-		{
-			return *internalString;
+		GlobalString(const GlobalString&) = default;
+		GlobalString(GlobalString&&) = default;
+		GlobalString& operator = (const GlobalString&) = default;
+		GlobalString& operator = (GlobalString&&) = default;
+
+		bool operator == (const GlobalString& other) const {
+			return stringId == other.stringId;
 		}
 
-		const char* cstr() const
-		{
-			return internalString->cstr();
+		/* Get an immutable reference to the global string referenced by this instance. 
+		Setting the template parameter to ThreadSafety::Unsafe will not lock the global string mutex. */
+		template<ThreadSafety safety = ThreadSafety::Safe>
+		const gk::String& toString() const {
+			if constexpr (safety == ThreadSafety::Safe) {
+				gk::LockedMutex<GlobalStringContainers> lock = getAllGlobalStrings().lock();
+				const GlobalStringContainers& containers = *lock;
+				return toStringFromGlobalContainers(containers);
+			}
+			// thread unsafe
+			const GlobalStringContainers& containers = getAllGlobalStrings().getDataNoLock();
+			return toStringFromGlobalContainers(containers);
 		}
 
-		size_t hash() const
-		{
-			return (size_t)internalString;
+		/* Calculate the hash value of this GlobalString using the pre-existing gk::hash<uint32>() specialization */
+		forceinline size_t hash() const {
+			return gk::hash(stringId);
+		}
+	
+	private:
+
+		static GlobalString createNewEntryIfDoesntExist(gk::String inString, GlobalStringContainers& containers) {
+			GlobalString gstrOut;
+			gk::Option<const uint32*> value = containers.ids.insert(inString, containers.nextId);
+
+			if (value.none()) {
+				gstrOut.stringId = containers.nextId;
+				containers.strings.Add(std::move(inString));
+				containers.nextId++;
+				return gstrOut;
+			}
+			gstrOut.stringId = *value.some();
+			return gstrOut;
 		}
 
-		/* std::cout << GlobalString */
-		friend std::ostream& operator << (std::ostream& os, const GlobalString& _string) {
-			return os << _string.cstr();
+		static GlobalString createFromExistingEntryIfExist(const gk::String& inString, const GlobalStringContainers& containers) {
+			GlobalString gstrOut;
+			gk::Option<const uint32*> value = containers.ids.find(inString);
+
+			if (value.none()) {
+				return gstrOut;
+			}
+			gstrOut.stringId = *value.some();
+			return gstrOut;
 		}
+
+		const gk::String& toStringFromGlobalContainers(const GlobalStringContainers& containers) const {
+			gk_assertm(stringId < containers.strings.Size(), "GlobalString stringId is not valid. Is outside of the range of the global strings array");
+			return containers.strings[stringId];
+		}
+
+	private:
+
+		uint32 stringId;
 
 	};
+
+	template<>
+	inline size_t hash<gk::GlobalString>(const gk::GlobalString& key) {
+		return key.hash();
+	}
 }
 
 namespace std
