a97d138cb645aa59fcde147efc0cc244e434832b
gabkhanfig <gabkhanfig@gmail.com>
2023-10-30 16:39:52 -0400

    Testing Nested Mutex

--- GkTypesLibTesting/Tests/MutexTests.cpp
+++ GkTypesLibTesting/Tests/MutexTests.cpp
@@ -15,6 +15,15 @@ void runIncrement(gk::Mutex<int>* mutex) {
 	}
 }
 
+void nestedLock(gk::Mutex<int>* mutex) {
+	for (int i = 0; i < 1000; i++) {
+		auto lock1 = mutex->lock();
+		auto lock2 = mutex->lock();
+		auto lock3 = mutex->lock();
+		(*lock2.get())++;
+	}
+}
+
 namespace UnitTests
 {
 	TEST(Mutex, SingleThreadsAccess) {
@@ -45,5 +54,23 @@ namespace UnitTests
 		t4.join();
 
 		EXPECT_EQ(*mutex.getDataNoLock(), 400);
+	}
+
+	TEST(Mutex, NestedLock) {
+		gk::Mutex<int> mutex = 0;
+
+		std::thread t1{nestedLock, & mutex};
+		std::thread t2{nestedLock, & mutex};
+		std::thread t3{nestedLock, & mutex};
+		std::thread t4{nestedLock, & mutex};
+
+		// If this test isn't completing, something is wrong.
+
+		t1.join();
+		t2.join();
+		t3.join();
+		t4.join();
+
+		EXPECT_EQ(*mutex.getDataNoLock(), 4000);
 	}
 }
