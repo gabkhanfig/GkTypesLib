d8880bebd3ed0c93ec11fbb18df2f8c71143d4ce
gabkhanfig <gabkhanfig@gmail.com>
2023-11-14 02:08:35 -0500

    Remove Spinlock Tests

--- GkTypesLibTesting/Tests/MutexTests.cpp
+++ GkTypesLibTesting/Tests/MutexTests.cpp
@@ -24,27 +24,6 @@ void nestedLock(gk::Mutex<int>* mutex) {
 	}
 }
 
-void incrementValueSpin(gk::Mutex<int>* mutex) {
-	auto lock = mutex->spinlock();
-	(*lock.get())++;
-}
-
-void runIncrementSpin(gk::Mutex<int>* mutex) {
-	Sleep(50);
-	for (int i = 0; i < 100; i++) {
-		incrementValueSpin(mutex);
-	}
-}
-
-void nestedLockSpin(gk::Mutex<int>* mutex) {
-	for (int i = 0; i < 1000; i++) {
-		auto lock1 = mutex->spinlock();
-		auto lock2 = mutex->spinlock();
-		auto lock3 = mutex->spinlock();
-		(*lock2.get())++;
-	}
-}
-
 struct NoDefaultConstructMutex {
 	int a;
 	int b;
@@ -95,54 +74,6 @@ namespace UnitTests
 		std::thread t2{nestedLock, & mutex};
 		std::thread t3{nestedLock, & mutex};
 		std::thread t4{nestedLock, & mutex};
-
-		// If this test isn't completing, something is wrong.
-
-		t1.join();
-		t2.join();
-		t3.join();
-		t4.join();
-
-		EXPECT_EQ(*mutex.getDataNoLock(), 4000);
-	}
-
-	TEST(Mutex, SingleThreadsAccessSpin) {
-		gk::Mutex<int> mutex = 0;
-
-		std::thread t1{runIncrementSpin, & mutex};
-
-		// If this test isn't completing, something is wrong.
-
-		t1.join();
-
-		EXPECT_EQ(*mutex.getDataNoLock(), 100);
-	}
-
-	TEST(Mutex, MultipleThreadsAccessSpin) {
-		gk::Mutex<int> mutex = 0;
-
-		std::thread t1{runIncrementSpin, & mutex};
-		std::thread t2{runIncrementSpin, & mutex};
-		std::thread t3{runIncrementSpin, & mutex};
-		std::thread t4{runIncrementSpin, & mutex};
-
-		// If this test isn't completing, something is wrong.
-
-		t1.join();
-		t2.join();
-		t3.join();
-		t4.join();
-
-		EXPECT_EQ(*mutex.getDataNoLock(), 400);
-	}
-
-	TEST(Mutex, NestedLockSpin) {
-		gk::Mutex<int> mutex = 0;
-
-		std::thread t1{nestedLockSpin, & mutex};
-		std::thread t2{nestedLockSpin, & mutex};
-		std::thread t3{nestedLockSpin, & mutex};
-		std::thread t4{nestedLockSpin, & mutex};
 
 		// If this test isn't completing, something is wrong.
 
