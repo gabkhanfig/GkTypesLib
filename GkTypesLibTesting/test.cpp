50ee5be491a48a869ecbfddd479f1a75c3271567
gabkhanfig <gabkhanfig@gmail.com>
2023-11-26 19:18:35 -0500

    About To Do Pr For CMake

--- GkTypesLibTesting/test.cpp
+++ GkTypesLibTesting/test.cpp
@@ -10,6 +10,23 @@
 #include "GkTest.h"
 #include "../GkTypesLib/GkTypes/Job/JobThread.h"
 
+#include "../GkTypesLib/GkTypes/Allocator/Allocator.h"
+#include "../GkTypesLib/GkTypes/Allocator/HeapAllocator.h"
+
+#include "../GkTypesLib/GkTypes/Array/ArrayList.h"
+
+#include <utility>
+#include "../GkTypesLib/GkTypes/Job/JobFuture.h"
+#include "../GkTypesLib/GkTypes/Job/JobSystem.h"
+
 int main(int argc, char** argv) {
-	runGkTests(argc, argv);
+	//runGkTests(argc, argv);
+
+	gk::ArrayList<int8> a;
+	for (int i = 0; i < 100; i++) {
+		a.push(i);
+	}
+	auto opt = a.find(80);
+	auto ind = opt.some();
+	std::cout << ind << std::endl;
 }
