5c2021e413ddb278f57979d86da101af4e735f64
gabkhanfig <gabkhanfig@gmail.com>
2023-12-26 00:25:27 -0500

    Fixed Masking Bug

--- gk_types_lib/utility.cpp
+++ gk_types_lib/utility.cpp
@@ -6,6 +6,6 @@ gk::Option<gk::usize> gk::bitscanForwardNext(u64* bitmask)
 {
 	unsigned long index;
 	if (!_BitScanForward64(&index, *bitmask)) return gk::Option<gk::usize>();
-	*bitmask = (*bitmask & ~(1ULL << index)) & 63;
+	*bitmask = (*bitmask & ~(1ULL << index));
 	return gk::Option<gk::usize>(index);
 }
