a8f686bc8f3e33b88735df0e6edb949062757c85
gabkhanfig <gabkhanfig@gmail.com>
2023-12-27 03:45:38 -0500

    Json Object Parse String

--- gk_types_lib/json/json_object.h
+++ gk_types_lib/json/json_object.h
@@ -49,9 +49,47 @@ namespace gk
 
 		constexpr ~JsonObject();
 
-		constexpr Option<JsonValue*> findField(const String& name);
+		/**
+		* Parse a string slice into a JsonObject, returning an Ok variant with the object.
+		* If the slice could not be successfully parsed, an Error variant is returned.
+		* 
+		* @param jsonString: String slice representing a json object.
+		* @return An Ok variant with the parsed JsonObject, or an Error.
+		*/
+		[[nodiscard]] static constexpr Result<JsonObject> parse(Str jsonString);
 
-		constexpr Option<const JsonValue*> findField(const String& name) const;
+		/**
+		* Convert this JsonObject into string representation following JSON formatting.
+		* 
+		* @return This JsonObject as a string.
+		*/
+		[[nodiscard]] constexpr String toString(usize nestCount = 0) const;
+
+		/**
+		* Find a mutable json value contained within this JsonObject.
+		* If it exists, a Some option containing the JsonValue will be returned,
+		* otherwise a None variant is returned.
+		* 
+		* NOTE: The JsonObject does not have pointer stability. Subsequent mutation operations on the JsonObject may
+		* invalidate the returned Some pointer due to the underlying data being moved to a new location.
+		* 
+		* @param name: The name/key of the field.
+		* @return The value if it exists, or None.
+		*/
+		[[nodiscard]] constexpr Option<JsonValue*> findField(const String& name);
+
+		/**
+		* Find an immutable json value contained within this JsonObject.
+		* If it exists, a Some option containing the JsonValue will be returned,
+		* otherwise a None variant is returned.
+		*
+		* NOTE: The JsonObject does not have pointer stability. Subsequent mutation operations on the JsonObject may
+		* invalidate the returned Some pointer due to the underlying data being moved to a new location.
+		*
+		* @param name: The name/key of the field.
+		* @return The value if it exists, or None.
+		*/
+		[[nodiscard]] constexpr Option<const JsonValue*> findField(const String& name) const;
 
 		/**
 		* Invalidates any iterators.
@@ -113,9 +151,18 @@ namespace gk
 		*/
 		constexpr Option<JsonValue*> addField(const String& name, const JsonValue& value);
 
+		/**
+		* Invalidates any iterators.
+		* Removes an entry from the JsonObject if it exists.
+		* 
+		* @return True if the entry does exist and was erased, or false if it doesn't exist. Can be ignored.
+		*/
 		constexpr bool eraseField(const String& name);
 
-		constexpr usize fieldCount() const { return elementCount; }
+		/**
+		* @return The number of fields within the JsonObject.
+		*/
+		[[nodiscard]] constexpr usize fieldCount() const { return elementCount; }
 
 		friend class Iterator;
 		friend class ConstIterator;
@@ -422,6 +469,11 @@ namespace gk
 			return _value.object;
 		}
 
+		/**
+		* Get the string representation of the json value.
+		*/
+		constexpr String toString(usize objectNestCount = 0) const;
+
 	private:
 
 		constexpr JsonValue(String&& inString)
@@ -520,8 +572,22 @@ namespace gk
 
 			void freeRuntime(Allocator* allocator);
 			
-		};
+		}; // struct JsonObjectBucket
 
+
+		constexpr bool isWhitespaceChar(char c);
+
+		constexpr Result<void> parseNullValue(usize* valueEnd, usize valueStart, const Str& jsonString);
+
+		constexpr Result<bool> parseBoolValue(usize* valueEnd, usize valueStart, const Str& jsonString);
+
+		constexpr Result<double> parseNumberValue(usize* valueEnd, usize valueStart, const Str& jsonString);
+
+		constexpr Result<String> parseStringValue(usize* valueEnd, usize valueStart, const Str& jsonString);
+
+		constexpr Result<ArrayList<JsonValue>> parseArrayValue(usize* valueEnd, usize valueStart, const Str& jsonString);
+
+		constexpr Result<JsonObject> parseObjectValue(usize* valueEnd, usize valueStart, const Str& jsonString);
 
 	}
 }
@@ -739,6 +805,37 @@ inline constexpr gk::JsonValue gk::JsonValue::makeObject(JsonObject&& object)
 {
 	JsonValue value = JsonValue(std::move(object));
 	return value;
+}
+
+inline constexpr gk::String gk::JsonValue::toString(usize objectNestCount) const
+{
+	constexpr i32 DECIMAL_PRECISION = 10;
+
+	switch (_type) {
+		case JsonValueType::Null:
+			return "null"_str;
+		case JsonValueType::Bool:
+			return String::fromBool(boolValue());
+		case JsonValueType::Number:
+			return String::fromFloat(numberValue(), DECIMAL_PRECISION);
+		case JsonValueType::String:
+			return String('\"' + stringValue() + '\"');
+		case JsonValueType::Array:
+			{
+				const ArrayList<JsonValue>& values = arrayValue();
+				String outString = '[';
+				for (usize i = 0; i < values.len(); i++) {
+					outString.append(values[i].toString());
+					if (i != (values.len() - 1)) {
+						outString.append(", "_str);
+					}
+				}
+				outString.append(']');
+				return outString;
+			}	
+		case JsonValueType::Object:
+			return objectValue().toString(objectNestCount + 1);
+	}
 }
 
 inline constexpr gk::internal::JsonObjectBucket::JsonObjectBucket(Allocator* allocator)
@@ -985,7 +1082,94 @@ constexpr gk::JsonObject::~JsonObject()
 		}
 		globalAllocator->freeBuffer(buckets, bucketCount);
 	}
+}
 
+inline constexpr gk::Result<gk::JsonObject> gk::JsonObject::parse(Str jsonString)
+{
+	if (jsonString.len < 2) {
+		return ResultErr(); // must be at least "{}"
+	}
+
+	// find start of object '{'
+	usize begin = 0;
+	{
+		if (internal::isWhitespaceChar(jsonString.buffer[0])) {
+			for (usize i = 1; i < jsonString.len; i++) {
+				if (internal::isWhitespaceChar(jsonString.buffer[i])) {
+					continue;
+				}
+				if (jsonString.buffer[i] == '{') {
+					begin = i;
+					break;
+				}
+				else {
+					return ResultErr(); // improperly formatted
+				}
+			}
+		}
+	}
+
+	// find end of object '}'
+	usize end = jsonString.len;
+	{
+		if (internal::isWhitespaceChar(jsonString.buffer[jsonString.len - 1])) {
+			for (usize i = jsonString.len; i > 0; i--) {
+				if (i == begin) {
+					return ResultErr(); // could not find '}' until it got back to the beginning
+				}
+
+				if (internal::isWhitespaceChar(jsonString.buffer[i - 1])) {
+					continue;
+				}
+				if (jsonString.buffer[i - 1] == '}') {
+					end = i;
+					break;
+				}
+				else {
+					return ResultErr(); // improperly formatted
+				}
+			}
+		}
+	}
+
+	if (begin != 0 || end != jsonString.len) { // if '{' and/or '}' are offset
+		jsonString = jsonString.substring(begin, end);
+	}
+
+	// at this point, jsonString has been isolated to just start with '{' and end with '}'
+	usize _ = ~0;
+	return internal::parseObjectValue(&_, 0, jsonString);
+}
+
+inline constexpr gk::String gk::JsonObject::toString(usize nestCount) const
+{
+	if (elementCount == 0) {
+		return "{}"_str;
+	}
+
+	String outString = '{';
+
+	usize i = 0;
+	for (ConstIterator::Pair pair : *this) {
+		const bool isLastIteration = i == (elementCount - 1);
+
+		outString.append('\n');
+		for (usize n = 0; n < (nestCount + 1); n++) {
+			outString.append("   ");
+		}
+		outString.append('\"' + pair.name + "\": "_str + pair.value.toString(nestCount));
+		if (!isLastIteration) {
+			outString.append(',');
+		}
+		i++;
+	}
+
+	outString.append('\n');
+	for (usize n = 0; n < nestCount; n++) {
+		outString.append("   ");
+	}
+	outString.append('}');
+	return outString;
 }
 
 inline constexpr gk::Option<gk::JsonValue*> gk::JsonObject::findField(const String& name)
@@ -1213,4 +1397,382 @@ inline constexpr void gk::JsonObject::ConstIterator::step() {
 			}
 		}
 	}
+}
+
+constexpr bool gk::internal::isWhitespaceChar(char c)
+{
+	constexpr char NEWLINE = '\n';
+	constexpr char CARRIAGE_RETURN = '\r';
+	constexpr char TAB = '\t';
+	constexpr char SPACE = ' ';
+
+	return c == NEWLINE || c == CARRIAGE_RETURN || c == TAB || c == SPACE;
+}
+
+constexpr gk::Result<void> gk::internal::parseNullValue(usize* valueEnd, usize valueStart, const Str& jsonString)
+{
+	for (usize valueIter = valueStart + 1; valueIter < jsonString.len; valueIter++) {
+		char c = jsonString.buffer[valueIter];
+
+		if (isWhitespaceChar(c) || c == ',' || c == '}') {
+			*valueEnd = valueIter;
+			const Str value = jsonString.substring(valueStart, *valueEnd);
+			if (value == "null"_str) {
+				return ResultOk<void>();
+			}
+			else {
+				return ResultErr();
+			}
+		}
+	}
+	return ResultErr();
+}
+
+constexpr gk::Result<bool> gk::internal::parseBoolValue(usize* valueEnd, usize valueStart, const Str& jsonString)
+{
+	for (usize valueIter = valueStart + 1; valueIter < jsonString.len; valueIter++) {
+		char c = jsonString.buffer[valueIter];
+		if (isWhitespaceChar(c) || c == ',' || c == ']' || c == '}') {
+			*valueEnd = valueIter;
+			const Str value = jsonString.substring(valueStart, *valueEnd);
+			return value.parseBool();
+		}
+		if (c >= 'a' || c <= 'z') {
+			continue;
+		}
+	}
+	return ResultErr();
+}
+
+constexpr gk::Result<double> gk::internal::parseNumberValue(usize* valueEnd, usize valueStart, const Str& jsonString)
+{
+	for (usize valueIter = valueStart + 1; valueIter < jsonString.len; valueIter++) {
+		char c = jsonString.buffer[valueIter];
+		if (c == '-') {
+			continue;
+		}
+		if (c >= '0' && c <= '9') {
+			continue;
+		}
+		else if (isWhitespaceChar(c) || c == ',' || c == ']' || c == '}') {
+			*valueEnd = valueIter;
+			const Str value = jsonString.substring(valueStart, *valueEnd);
+			return value.parseFloat();
+		}
+	}
+	return ResultErr();
+}
+
+constexpr gk::Result<gk::String> gk::internal::parseStringValue(usize* valueEnd, usize valueStart, const Str& jsonString)
+{
+	for (usize valueIter = valueStart + 1; valueIter < jsonString.len; valueIter++) {
+		char c = jsonString.buffer[valueIter];
+		
+		if (c == '\"' && jsonString.buffer[valueIter - 1] != '\\') {
+			*valueEnd = valueIter + 1;
+			const Str value = jsonString.substring(valueStart + 1, *valueEnd - 1);
+			return ResultOk<String>(String(value));
+		}
+	}
+	return ResultErr();
+}
+
+constexpr gk::Result<gk::ArrayList<gk::JsonValue>> gk::internal::parseArrayValue(usize* valueEnd, usize valueStart, const Str& jsonString)
+{
+	ArrayList<JsonValue> accumulate;
+
+	usize valueIter = valueStart + 1;
+	while (valueIter < jsonString.len) {
+		char c = jsonString.buffer[valueIter];
+
+		if (isWhitespaceChar(c)) {
+			valueIter++;
+			continue;
+		}
+
+
+		usize currentValueStart = valueIter;
+		usize currentValueEnd = ~0;
+		bool didParseCurrentValue = false;
+		JsonValueType hint = JsonValueType::Null;
+
+		if (c == 'n') {
+			hint = JsonValueType::Null;
+			Result<void> value = parseNullValue(&currentValueEnd, currentValueStart, jsonString);
+			if (!value.isError()) {
+				accumulate.push(JsonValue::makeNull());
+				didParseCurrentValue = true;
+			}
+		}
+		else if (c == 't' || c == 'f') {
+			hint = JsonValueType::Bool;
+			Result<bool> value = parseBoolValue(&currentValueEnd, currentValueStart, jsonString);
+			if (!value.isError()) {
+				accumulate.push(JsonValue::makeBool(value.ok()));
+				didParseCurrentValue = true;
+			}
+		}
+		else if (c >= '0' && c <= '9') {
+			hint = JsonValueType::Number;
+			Result<double> value = parseNumberValue(&currentValueEnd, currentValueStart, jsonString);
+			if (!value.isError()) {
+				accumulate.push(JsonValue::makeNumber(value.ok()));
+				didParseCurrentValue = true;
+			}
+		}
+		else if (c == '-') {
+			hint = JsonValueType::Number;
+			Result<double> value = parseNumberValue(&currentValueEnd, currentValueStart, jsonString);
+			if (!value.isError()) {
+				accumulate.push(JsonValue::makeNumber(value.ok()));
+				didParseCurrentValue = true;
+			}
+		}
+		else if (c == '\"') {
+			hint = JsonValueType::String;
+			Result<String> value = parseStringValue(&currentValueEnd, currentValueStart, jsonString);
+			if (!value.isError()) {
+				accumulate.push(JsonValue::makeString(value.ok()));
+				didParseCurrentValue = true;
+			}
+		}
+		else if (c == '[') {
+			hint = JsonValueType::Array;
+			Result<ArrayList<JsonValue>> value = parseArrayValue(&currentValueEnd, currentValueStart, jsonString);
+			if (!value.isError()) {
+				accumulate.push(JsonValue::makeArray(value.ok()));
+				didParseCurrentValue = true;
+			}
+		}
+		else if (c == '{') {
+			hint = JsonValueType::Object;
+			check(false);
+			break;
+		}
+
+
+		if (!didParseCurrentValue) {
+			return ResultErr();
+		}
+
+		for (usize nextValueIter = currentValueEnd; nextValueIter < jsonString.len; nextValueIter++) {
+			if (isWhitespaceChar(jsonString.buffer[nextValueIter])) {
+				continue;
+			}
+			if (jsonString.buffer[nextValueIter] == ',') {
+				valueIter += (nextValueIter - valueIter) + 1;			
+				break;
+			}
+			if (jsonString.buffer[nextValueIter] == ']') {
+				*valueEnd = nextValueIter + 1;
+				return ResultOk<ArrayList<JsonValue>>(std::move(accumulate));
+			}
+
+			return ResultErr();		
+		}
+	}
+	return ResultErr();
+}
+
+constexpr gk::Result<gk::JsonObject> gk::internal::parseObjectValue(usize* valueEnd, usize valueStart, const Str& jsonString)
+{
+	JsonObject accumulate;
+
+	usize valueIter = valueStart + 1;
+	while (valueIter < jsonString.len) {
+		char c = jsonString.buffer[valueIter];
+
+		if (isWhitespaceChar(c)) {
+			valueIter++;
+			continue;
+		}
+		else if (c == '}') { // end of object
+			return ResultOk<JsonObject>(accumulate);
+		}
+		else if (c != '\"') {
+			return ResultErr();
+		}
+
+		const usize nameStart = valueIter + 1;
+		usize nameEnd = ~0;
+		for (usize nameIter = nameStart; nameIter < jsonString.len; nameIter++) {
+			if (jsonString.buffer[nameIter] == '\"' && jsonString.buffer[nameIter - 1] != '\\') {
+				nameEnd = nameIter;
+				break;
+			}
+		}
+		if (nameEnd == ~0) { // could not get to end of name
+			return ResultErr();
+		}
+
+		const Str name = jsonString.substring(nameStart, nameEnd);
+
+		usize delimiterPosition = ~0;
+		for (usize delimiterIter = nameEnd + 1; delimiterIter < jsonString.len; delimiterIter++) {
+			if (internal::isWhitespaceChar(jsonString.buffer[delimiterIter])) {
+				continue;
+			}
+			if (jsonString.buffer[delimiterIter] == ':') {
+				delimiterPosition = delimiterIter;
+				break;
+			}
+			else {
+				return ResultErr();
+			}
+		}
+		if (delimiterPosition == ~0) { // could not find delimiter
+			return ResultErr();
+		}
+		
+		usize currentValueStart = ~0;
+		JsonValueType hint = JsonValueType::Null;
+		for (usize valueStartIter = delimiterPosition + 1; valueStartIter < jsonString.len; valueStartIter++) {
+			char c = jsonString.buffer[valueStartIter];
+			if (internal::isWhitespaceChar(c)) {
+				continue;
+			}
+			if (c == 'n') {
+				currentValueStart = valueStartIter;
+				hint = JsonValueType::Null;
+				break;
+			}
+			if (c == 't' || c == 'f') {
+				currentValueStart = valueStartIter;
+				hint = JsonValueType::Bool;
+				break;
+			}
+			if (c >= '0' && c <= '9') {
+				currentValueStart = valueStartIter;
+				hint = JsonValueType::Number;
+				break;
+			}
+			if (c == '-') {
+				currentValueStart = valueStartIter;
+				hint = JsonValueType::Number;
+				break;
+			}
+			if (c == '\"') {
+				currentValueStart = valueStartIter;
+				hint = JsonValueType::String;
+				break;
+			}
+			if (c == '[') {
+				currentValueStart = valueStartIter;
+				hint = JsonValueType::Array;
+				break;
+			}
+			if (c == '{') {
+				currentValueStart = valueStartIter;
+				hint = JsonValueType::Object;
+				break;
+			}
+			else {
+				return ResultErr();
+			}
+		}
+		if (currentValueStart == ~0) { // could not find start of value
+			return ResultErr();
+		}
+
+		usize currentValueEnd = ~0;
+
+		switch (hint) {
+
+		case JsonValueType::Null:
+		{
+			Result<void> value = internal::parseNullValue(&currentValueEnd, currentValueStart, jsonString);
+			if (value.isError()) {
+				return ResultErr();
+			}
+			else {
+				accumulate.addField(String(name), JsonValue::makeNull());
+			}
+		}
+		break;
+
+		case JsonValueType::Bool:
+		{
+			Result<bool> value = internal::parseBoolValue(&currentValueEnd, currentValueStart, jsonString);
+			if (value.isError()) {
+				return ResultErr();
+			}
+			else {
+				accumulate.addField(String(name), JsonValue::makeBool(value.ok()));
+			}
+		}
+		break;
+
+		case JsonValueType::Number:
+		{
+			Result<double> value = internal::parseNumberValue(&currentValueEnd, currentValueStart, jsonString);
+			if (value.isError()) {
+				return ResultErr();
+			}
+			else {
+				accumulate.addField(String(name), JsonValue::makeNumber(value.ok()));
+			}
+		}
+		break;
+
+
+		case JsonValueType::String:
+		{
+			Result<String> value = internal::parseStringValue(&currentValueEnd, currentValueStart, jsonString);
+			if (value.isError()) {
+				return ResultErr();
+			}
+			else {
+				accumulate.addField(String(name), JsonValue::makeString(value.ok()));
+			}
+		}
+		break;
+
+		case JsonValueType::Array:
+		{
+			Result<ArrayList<JsonValue>> value = internal::parseArrayValue(&currentValueEnd, currentValueStart, jsonString);
+			if (value.isError()) {
+				return ResultErr();
+			}
+			else {
+				accumulate.addField(String(name), JsonValue::makeArray(value.ok()));
+			}
+		}
+		break;
+
+		case JsonValueType::Object:
+		{
+			Result<JsonObject> value = internal::parseObjectValue(&currentValueEnd, currentValueStart, jsonString);
+			if (value.isError()) {
+				return ResultErr();
+			}
+			else {
+				accumulate.addField(String(name), JsonValue::makeObject(value.ok()));
+			}
+		}
+		break;
+		}
+		if (currentValueEnd == ~0) {
+			return ResultErr();
+		}
+
+		for (usize nextStart = currentValueEnd; nextStart < jsonString.len; nextStart++) {
+			char c = jsonString.buffer[nextStart];
+
+			if (internal::isWhitespaceChar(c)) {
+				continue;
+			}
+			else if (c == ',') {
+				valueIter += (nextStart - valueIter) + 1;
+				break;
+			}
+			else if (c == '}') {
+				*valueEnd = nextStart + 1;
+				return ResultOk<JsonObject>(accumulate);
+			}
+			else {
+				return ResultErr();
+			}
+		}
+	}
+	return ResultErr();
 }
